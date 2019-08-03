18788456ae255a97ece36a5a8e5fe30d26c30597
JasonkayZK <271226192@qq.com>
2019-08-03 11:44:16 +0800

    创建新的工程仓库

---
+++ README.md
@@ -0,0 +1,1 @@
+A repository for learning algorithm using Java.
---
+++ src/main/java/algorithm/math/GCD.java
@@ -0,0 +1,23 @@
+package algorithm.math;
+
+public class GCD {
+
+    /**
+     * 阿基米德算法， 计算两个数的最大公约数
+     *
+     * @param p 整数p
+     * @param q 整数q
+     * @return p, q的最大公约数
+     */
+    public static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        int r = p % q;
+        return gcd(q, r);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(5, 115));
+    }
+}
